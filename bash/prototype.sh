ebe757762d963ae38cd88b3c817fe5c3534fd7ca
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2012-11-16 14:58:05 +0000

    Basic menu

--- bash/prototype.sh
+++ bash/prototype.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+function company {
+	echo "You are a company."
+}
+function courseCoordinator {
+	echo "You are a course coordinator."
+}
+function student {
+	echo "You are a student."
+}
+PS3="Who are you? "
+options=("Company" "Course Coordinator" "Student" "Quit")
+select opt in "${options[@]}"
+do
+	case $opt in
+    "Company")
+		company;;
+   	"Course Coordinator")
+		courseCoordinator;;
+	"Student")
+		student;;
+    "Quit")
+		exit;;
+	*)
+		echo "You are an alien.";;
+	esac
+done
