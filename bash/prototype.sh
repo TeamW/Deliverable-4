609fbc38632fbe3d12d2143c97e7208e0bb75d4a
ryaanwells <ryaanwells@googlemail.com>
2012-11-26 10:39:03 +0000

    Finished bash prototype

--- bash/prototype.sh
+++ bash/prototype.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 options=("Company" "Course Coordinator" "Student" "Quit")
-compopt=("Add new advertisement" "Amend application" "Quit")
+compopt=("Add new advertisement" "Amend advertisement" "Quit")
 studopt=("View advertisements" "Declare placement success" "Quit")
 cocoopt=("View pending advertisements" "View new student declarations" "Quit")
-addopt=("Accept" "Decline")
-advert=""
+addopt=("Accept" "Decline" "Postpone")
+usropt=("Next advertisement" "Quit")
 
 function addAdvert {
     echo "Add the text of your advertisement here"
@@ -34,9 +34,9 @@ function company {
 		clear
 		echo Advert added successfully.
 		break;;
-	    "Amend application")
+	    "Amend advertisement")
 		clear
-		echo "This is where you can amend shit"
+		echo "This is a stub. Functionality of amending an advertisement will go here"
 		break;;
 	    "Quit")
 		clear
@@ -44,16 +44,19 @@ function company {
 		PS3="Who are you? "
 		return;;
 	    *)
+		clear
 		echo "Sorry, this command was not recognised"
 		break;;
 	esac
     done
     done
 }
+
 function courseCoordinator {
     echo "You are a Course Coordinator."
     PS3="What do you wish to do, Course Coordinator? "
     while(true) do
+	quickView
     select opt in "${cocoopt[@]}"
     do
 	case $opt in
@@ -63,7 +66,7 @@ function courseCoordinator {
 		break;;
 	    "View new student declarations")
 		clear
-		echo "This is where you can see shithot students"
+		echo "This is a stub. Functionality of viewing new student declarations will go here."
 		break;;
 	    "Quit")
 		clear
@@ -71,12 +74,14 @@ function courseCoordinator {
 		PS3="Who are you? "
 		return;;
 	    *)
+		clear
 		echo "Sorry, this command was not recognised"
 		break;;
 	esac
     done
     done
 }
+
 function student {
     echo "You are a student."
     PS3="What do you wish to do, Student? "
@@ -90,7 +95,7 @@ function student {
 		break;;
 	    "Declare placement success")
 		clear
-		echo "This is where you can say you're shithot"
+		echo "This is a stub. Functionality of viewing new student declarations will go here."
 		break;;
 	    "Quit")
 		clear
@@ -98,6 +103,7 @@ function student {
 		PS3="Who are you? "
 		return;;
 	    *)
+		clear
 		echo "Sorry, this command was not recognised"
 		break;;
 	esac
@@ -105,15 +111,19 @@ function student {
     done
 
 }
+
 function viewAdvertisements {
 	fileList=""
-	for file in $(ls .sespadv)
+	for file in $(ls .sespadv/)
 	do
 		fileList="$fileList .sespadv/$file"
 	done
 	array=($fileList)
 	size=${#array[@]}
-	echo $size
+	if [ $size -eq 0 ] 
+		then
+		echo "Sorry, there are no advertisements to view at this time"
+	fi
 	for file in ${array[*]}
 	do
 		while read line; do
@@ -122,14 +132,29 @@ function viewAdvertisements {
 		echo '
 Do you want to apply for this? If so, please contact the company.
 '
+		select opt in "${usropt[@]}"
+		do
+		case $opt in
+			"Next advertisement")
+				clear
+				break;;
+			"Quit")
+				clear
+				return;;
+			*)
+			echo "Sorry, this command was not recognised"
+		esac
+		done
 	done
 }
+
 function pendingAdvertisements {
-	for file in $(ls .sesppend)
+	for file in $(ls .sesppend/)
 	do
 		while read line; do
 			echo "$line"
 		done < ".sesppend/$file"
+		echo
 		select opt in "${addopt[@]}"
 		do
 		case $opt in
@@ -140,19 +165,38 @@ function pendingAdvertisements {
 			mv ".sesppend/$file" ".sespdecl/$file"
 			clear
 			break;;
+			"Postpone")
+			clear
+			break;;
 			*)
-			echo "Sorry your option is unrecognised."
+			echo "Sorry, your option is unrecognised."
 			break;;
 		esac
 		done
 	done
+	clear
+	echo "All advertisements dealt with."
 }
+
+function quickView {
+	pendcount=`ls .sesppend | wc -l`
+	livecount=`ls .sespadv | wc -l`
+	refcount=`ls .sespdecl | wc -l`
+	echo "
+Today at a glance
+-----------------"
+	echo "Pending advertisements: $pendcount"
+	echo "Live advertisements: $livecount"
+	echo "Refused Advertisements: $refcount
+"
+}
+
 clear
 for line in $(cat .sesp.conf)
 do
 adno=$line
-echo $adno
-done<.sesp.conf
+done
+echo "Welcome to the system, please log in."
 PS3="Who are you? "
 while(true) do
 select opt in "${options[@]}"
